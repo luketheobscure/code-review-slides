88707192dad4c9e904daf10a23548ef13cf08c4c
Luke Deniston <ldeniston@gofundme.com>
2026-05-27 11:32:50 -0700

    final slide

--- src/main.js
+++ src/main.js
@@ -37,12 +37,13 @@ import s22 from './slides/26-closing.html?raw';
 import s23 from './slides/27-incentivize-review.html?raw';
 import s24 from './slides/28-gen-review-bar.html?raw';
 import s25 from './slides/29-summary.html?raw';
+import s26 from './slides/30-thanks.html?raw';
 
 const slides = [
   s00, s01, s02, s03, s04, s05, s06, s06b, s07, s08, s09,
   s10, s11, s12, s13, s14, s15, s16, s17d, s17b, s17c, s17e, s23,
   s18, s19, s20, s21, s24,
-  s25, s22,
+  s25, s22, s26,
 ];
 
 document.querySelector('.slides').innerHTML = slides.join('\n');
